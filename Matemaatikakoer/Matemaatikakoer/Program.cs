62981960b58b5a2bc20b261fbfde939702c00037
Kristoferlatt <42769298+Kristoferlatt@users.noreply.github.com>
2023-01-12 19:30:04 +0200

    Add files via upload

---
+++ Matemaatikakoer/Matemaatikakoer/Program.cs
@@ -0,0 +1,79 @@
+﻿namespace Matemaatikakoer
+{
+    internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Matemaatikakoer");
+
+        Console.WriteLine("Palun sisesta esimene arv");
+        float firstNr = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Palun sisesta tehe: ");
+        string operation = Console.ReadLine();
+
+        Console.WriteLine("Palun sisesta teine arv");
+        float secondNr = int.Parse(Console.ReadLine());
+
+
+
+        try
+        {
+            switch (operation)
+            {
+                case "+":
+                    float resultAdd = firstNr + secondNr;
+                    Console.WriteLine("Kahe numbri liitmisel on vastus: " + resultAdd);
+                    break;
+
+                default:
+                    break;
+
+            }
+                switch (operation)
+                {
+                    case "-":
+                        float resultAdd = firstNr - secondNr;
+                        Console.WriteLine("Kahe numbri lahutamisel on vastus: " + resultAdd);
+                        break;
+
+                    default:
+                        break;
+
+                }
+                switch (operation)
+                {
+                    case "/":
+                        float resultAdd = firstNr / secondNr;
+                        Console.WriteLine("Kahe numbri jagamisel on vastus: " + resultAdd);
+                        break;
+
+                    default:
+                        break;
+
+                }
+                switch (operation)
+                {
+                    case "*":
+                        float resultAdd = firstNr * secondNr;
+                        Console.WriteLine("Kahe numbri korrutamisel on vastus: " + resultAdd);
+                        break;
+
+                    default:
+                        break;
+
+                }
+            }
+        catch (FormatException)
+        {
+            Console.WriteLine("Vale numbri formaat.");
+            //throw1;
+            //123
+        }
+        catch (OverflowException)
+        {
+            Console.WriteLine("ERROR. Sisestatud number on liiga suur.");
+        }
+    }
+}
+}
